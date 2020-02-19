29a948cf256942c107b5c548165f55d82f918e4a
fekete20 <fekete20@iit353.iit.uni-miskolc.hu>
2020-02-19 15:30:59 +0100

    .

--- 01/tanulo-vasarlas/src/main/java/hu/me/krz/haladojava/Vasarlas.java
+++ 01/tanulo-vasarlas/src/main/java/hu/me/krz/haladojava/Vasarlas.java
@@ -1,31 +1,43 @@
 package hu.me.krz.haladojava;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
+
+import hu.me.krz.haladojava.*;
 
 public class Vasarlas {
 	public static void main(String[] args) {
-		int n = 0;
-		
-		List<Tanulo> tanulok = new ArrayList<Tanulo>();
 		
 		
-		
-	//	tanulok.add(arg0)
-		
-		
+		List<Tanulo> tanulok = new ArrayList<>(Arrays.asList(new Tanulo("Katalin"), new Tanulo("István"),
+				new Tanulo("Péter"), new Tanulo("Gréta"), new Tanulo("Lilla")));
+
+		Random r = new Random();
+		for (int i = 0; i <= tanulok.size(); i++) {
+			
+			tanulok.get(i).fogyasztas(r.nextInt((3000 - 1500) + 1) + 1500);
+
+			System.out.println("Maradt tanulók száma: " + tanulok.size()) ;
+			
+			for(Tanulo t : tanulok) {
+				System.out.println(t);
+			}
+		}
+
 	}
-	
-	
+
 }
 
 /*
-Hozzon létre egy dinamikus tömböt, melyben Tanulo példányok tárolhatók!
-Példányosítson 5 db objektumot, tetszőleges adattokkal, használja az egy paraméteres konstruktort, majd illessze
- be a tömbbe!
-Hadd szaladjon a vásárlás! 6 körös vásárlást szimulálunk. Ciklusban (6 iteráció) hívja meg a fogyasztás
- metódust minden Tanulo objektumra, véletlen értékekkel (a 1500-3000 intervallumból).
-A visszatérési értéket vizsgálva nullázza ki azokat a Tanulo –kat amelyeknek elfogyott a pénzük!
-Minden körben írja ki az átlag pénz értékét (összes pénz / tanulok száma)!
-Írassa ki hány Tanulo maradt a vásárlás végén, majd írassa ki a tanulókat (toString)!
-*/
+ * Hozzon létre egy dinamikus tömböt, melyben Tanulo példányok tárolhatók!
+ * Példányosítson 5 db objektumot, tetszőleges adattokkal, használja az egy
+ * paraméteres konstruktort, majd illessze be a tömbbe! Hadd szaladjon a
+ * vásárlás! 6 körös vásárlást szimulálunk. Ciklusban (6 iteráció) hívja meg a
+ * fogyasztás metódust minden Tanulo objektumra, véletlen értékekkel (a
+ * 1500-3000 intervallumból). A visszatérési értéket vizsgálva nullázza ki
+ * azokat a Tanulo –kat amelyeknek elfogyott a pénzük! Minden körben írja ki az
+ * átlag pénz értékét (összes pénz / tanulok száma)! Írassa ki hány Tanulo
+ * maradt a vásárlás végén, majd írassa ki a tanulókat (toString)!
+ */
