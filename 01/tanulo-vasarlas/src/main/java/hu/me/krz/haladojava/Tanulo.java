1c3ec72d92908ecc34debbbc3c7dc72c724df163
fekete20 <fekete20@iit353.iit.uni-miskolc.hu>
2020-02-19 14:14:44 +0100

    .

--- 01/tanulo-vasarlas/src/main/java/hu/me/krz/haladojava/Tanulo.java
+++ 01/tanulo-vasarlas/src/main/java/hu/me/krz/haladojava/Tanulo.java
@@ -2,52 +2,47 @@ package hu.me.krz.haladojava;
 
 public class Tanulo {
 
-	/*
-	 * 1. Definiáljon egy **Tanulo** osztályt, amely tárolja a **nev** (szöveg) és a **penz** (egész) adatokat. A
-	 *  név adatot csak egyszer szeretnénk beállítani a konstruktorban! 
-Az adatokat a leszármazott osztályból és az adott osztályból szeretnénk csak kiolvasni!
-    * Definiáljon egy megfelelő adattagot (tanulok), amely az összes Tanulo objektum darabszámát képes tárolni.
-    * Készítsen megfelelő gettert osszestanulo néven, amely a tanulok darabszámát adja vissza!
-    * Készítsen csak gettert penz néven, amely a penz értékét adja vissza!
-    * Definiáljon konstruktort, amellyel egy példány létrehozható! 
-    Gondoskodjon a tanulok adattag frissítéséről!
-    * Definiáljon egy egy paraméteres konstruktort, amely meghívja az előző konstruktort, a nevet a 
-    * paraméterrel, 
-    a pénzt pedig 10000-re állítja be.
-    * Definiáljon egy megfelelő metódust arra az esetre, ha az objektum megszűnne, frissítse a tanulok 
-    * adattagot.
-    * Definiálja a toString metódust úgy, hogy a következő formában adja vissza az objektum állapotát:
-    *  Tanuló: [név] 1301 Ft;
-    * Definiáljon egy fogyasztás metódust, amely a paraméterben kapott értékkel csökkenti a pénz adattagot! 
-    * Térjen vissza igaz értékkel, ha a pénz elfogyott, hamissal egyébként!
-	 */
-	
 	private final String name;
 	private int money;
-	private int tanulok;
-	
+	private int tanulok = 0;
+
 	public int osszestanulo() {
 		return tanulok;
 	}
-	
+
 	public int penz() {
 		return money;
 	}
 
-	public Tanulo(String name, int money, int tanulok) {
-		//super();
+	public Tanulo(String name, int money) {
+		// super();
 		this.name = name;
 		this.money = money;
-		this.tanulok = tanulok;
+		tanulok++;
 	}
-	
+
 	public Tanulo(String name) {
-		this(name);
-		money = 
+		this(name, 10000);
 	}
-	
-	
-	
-	
-	
+
+	@Override
+	protected void finalize() throws Throwable {
+		super.finalize();
+		tanulok--;
+	}
+
+	@Override
+	public String toString() {
+		return "Tanuló: " + name + ", " + money;
+	}
+
+	public boolean fogyasztas(int dec) {
+		this.money -= dec;
+
+		if (money <= 0) {
+			return true;
+		} else
+			return false;
+	}
+
 }
---
+++ 01/tanulo-vasarlas/src/main/java/hu/me/krz/haladojava/Vasarlas.java
@@ -0,0 +1,31 @@
+package hu.me.krz.haladojava;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Vasarlas {
+	public static void main(String[] args) {
+		int n = 0;
+		
+		List<Tanulo> tanulok = new ArrayList<Tanulo>();
+		
+		
+		
+	//	tanulok.add(arg0)
+		
+		
+	}
+	
+	
+}
+
+/*
+Hozzon létre egy dinamikus tömböt, melyben Tanulo példányok tárolhatók!
+Példányosítson 5 db objektumot, tetszőleges adattokkal, használja az egy paraméteres konstruktort, majd illessze
+ be a tömbbe!
+Hadd szaladjon a vásárlás! 6 körös vásárlást szimulálunk. Ciklusban (6 iteráció) hívja meg a fogyasztás
+ metódust minden Tanulo objektumra, véletlen értékekkel (a 1500-3000 intervallumból).
+A visszatérési értéket vizsgálva nullázza ki azokat a Tanulo –kat amelyeknek elfogyott a pénzük!
+Minden körben írja ki az átlag pénz értékét (összes pénz / tanulok száma)!
+Írassa ki hány Tanulo maradt a vásárlás végén, majd írassa ki a tanulókat (toString)!
+*/
